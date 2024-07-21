38d505bacff4743886fd6aede9cb94e9f1bdfc59
Louise White <louise.white2@engineering.digital.dwp.gov.uk>
2024-07-21 15:07:33 +0100

    added versions for availability work for has, vs and afn
    GitOrigin-RevId: 2a01f1057e544cb6f094b5d352cddf28ec5aef83

--- app/views/v1-rb/_routes.js
+++ app/views/v1-rb/_routes.js
@@ -4,14 +4,38 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here - above the module.exports line
 
 
-router.post('/availability-management/manage-select', function (req, res) {
+router.post('/has/availability-management/manage-select', function (req, res) {
 
   var manage = req.session.data['manage']
 
   if (manage == "agents"){
     res.redirect('agents/agent-profiles')
   } else {
-    res.redirect('/v1-rb/availability-management/availability/availability-view')
+    res.redirect('/v1-rb/has/availability-management/availability/availability-view')
+  }
+
+})
+
+router.post('/afn/availability-management/manage-select', function (req, res) {
+
+  var manage = req.session.data['manage']
+
+  if (manage == "agents"){
+    res.redirect('agents/agent-profiles')
+  } else {
+    res.redirect('/v1-rb/afn/availability-management/availability/availability-view')
+  }
+
+})
+
+router.post('/vs/availability-management/manage-select', function (req, res) {
+
+  var manage = req.session.data['manage']
+
+  if (manage == "agents"){
+    res.redirect('agents/agent-profiles')
+  } else {
+    res.redirect('/v1-rb/vs/availability-management/availability/availability-view')
   }
 
 })
